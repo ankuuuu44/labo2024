34922af088005e179bd496aef5b7b12d2b4f90c0
Tomoya <36haruyamap@gmail.com>
2024-05-20 05:46:13 +0900

    選択された特徴量をまとめたcsvファイル生成

--- sien/machineLearningsample.php
+++ sien/machineLearningsample.php
@@ -27,16 +27,25 @@
     ?>
     <?php
         if(isset($_POST['featureLabel'])){
-            $featurevalue_sql = "SELECT UID,WID,Understand,";
+            $allresult = array();
+            $featurevalue_sql = "SELECT ";
+
+            //作業用
+            $column_name = "UID,WID,Understand,";
             $selectcolumn = implode(",", $_POST['featureLabel']);
-            $featurevalue_sql.= $selectcolumn." FROM featurevalue";
+            $column_name.= $selectcolumn;   //データベースの列名が入っている．
+            $featurevalue_sql.= $column_name." FROM featurevalue";
             $featurevalue_res = mysqli_query($conn, $featurevalue_sql);
             echo "選択した特徴量は".$selectcolumn."です<br>";
             echo "生成したSQLは".$featurevalue_sql."です<br>";
+
+            
             if($featurevalue_res != false){
                 $featurevalue_res_numrows = mysqli_num_rows($featurevalue_res);
                 echo "抽出したデータ数は",$featurevalue_res_numrows;
             }
+            $fp = fopen('./pydata/test.csv', 'w');
+            fputcsv($fp, explode(',', $column_name));
 
             //動的なテーブル生成
             echo "<table class='tableclassname' border='1'>";
@@ -48,21 +57,46 @@
             //ここまで動的なテーブル生成
 
             while($featurevalue_rows = $featurevalue_res -> fetch_assoc()){
+                $allresult[] = $featurevalue_rows;  //全部のデータをallresultに入れる．後々引数で使う．
                 //ここに$POST[featureLabel]に応じてカラム名を動的に変化させる．
                 echo "<tr><td>{$featurevalue_rows['UID']}</td>",
                     "<td>{$featurevalue_rows['WID']}</td>",
                     "<td>{$featurevalue_rows['Understand']}</td>";
+
                 foreach($_POST['featureLabel'] as $addcolumnname){
                     echo "<td>{$featurevalue_rows[$addcolumnname]}</td>";
                 }
                 echo "</tr>";
-            } 
+            }
+            echo "</table>";
+
+            
+
+            //print_r($allresult);
             
 
         }else{
             echo "特徴量を指定してください．";
         }
+        //結果をcsvファイルに記述
+        $fp1 = fopen('./pydata/test.csv', 'a');
+        foreach ($allresult as $fields) {
+            fputcsv($fp1, $fields);
+        }
+        fclose($fp);
         
+    ?>
+
+    <?php
+        //Pythonに渡すプログラム
+        /*
+        $pyScript = "php_machineLearning.py";
+        $cmd = array("py",$pyScript,escapeshellarg(json_encode($allresult)));
+        $cmdText = implode(" ",$cmd);
+        echo "Running command: " . $cmdText . "\n";
+        */
+        //exec("py", $output);
+
     ?>
 
 
